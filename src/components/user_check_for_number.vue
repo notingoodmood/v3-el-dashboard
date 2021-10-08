1d7d337c72f387696aed33d55cfe9eaa6f652d30
lvzhiwei02 <lvzhiwei02@meituan.com>
2021-10-08 19:21:55 +0800

    1.1

--- src/components/user_check_for_number.vue
+++ src/components/user_check_for_number.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
-    请输入从经销商处得到的工单代码(bill code)：
-    <input type="text" v-model="inputSome" style="width:300px;" />
-
-    <el-button type="text" @click="checkForData" style="margin-left:20px"
-      >查询</el-button
-    >
-
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
-      <span>正在查询...</span>
+    <div style="margin-top:10px;margin-bottom:10px;">
+      请输入从经销商处得到的工单代码(bill code)：
+      <input type="text" v-model="inputSome" style="width:300px;" />
+      <el-button type="text" @click="checkForData" style="margin-left:20px"
+        >查询分属手机号码</el-button
+      >
+    </div>
+    <div v-if="this.selectRow" style="margin-top:10px;margin-bottom:10px">
+      当前选定手机号码：{{ this.selectRow.phone_number }}
+      <el-button type="text" @click="checkForData" style="margin-left:20px"
+        >查询该手机号码短信</el-button
+      >
+    </div>
+    <el-dialog border title="提示" :visible.sync="dialogVisible" width="30%">
+      <span>{{ dialog_text }}</span>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">强制停止</el-button>
+        <el-button @click="dialogVisible = false">{{ button_text }}</el-button>
       </span>
     </el-dialog>
 
-    <vxe-table align="allAlign" :data="tableData" empty-text="无数据">
-      <vxe-column type="seq" width="60"></vxe-column>
+    <vxe-table
+      align="allAlign"
+      :data="tableData"
+      empty-text="无数据"
+      :radio-config="{ highlight: true }"
+      @radio-change="radioChangeEvent"
+    >
+      <vxe-column type="radio" width="60">
+        <template #header>
+          <vxe-button type="text" :disabled="true">选定</vxe-button>
+        </template>
+      </vxe-column>
       <vxe-column field="phone_number" title="短信号码" sortable></vxe-column>
     </vxe-table>
   </div>
@@ -31,12 +47,18 @@ export default {
       inputSome: "",
       dialogVisible: false,
       tableData: [],
+      dialog_text: params.dialog_loading_text,
+      button_text: params.dialog_button_loading,
+      selectRow: null,
     };
   },
   methods: {
     checkForData() {
       if (this.dialogVisible == false) {
+        this.dialog_text = params.dialog_loading_text;
+        this.button_text = params.dialog_button_loading;
         this.dialogVisible = true;
+        this.selectRow = null;
       }
       ajax({
         methods: "GET",
@@ -45,22 +67,31 @@ export default {
           bill_code: this.inputSome,
         },
         success: (res) => {
-          this.dialogVisible = false;
+          this.tableData = [];
           if (res.statusCode === 0) {
-            let array = [];
-            for (let s of res.data.list) {
-              array.push({ phone_number: s });
-            }
-            this.tableData = array;
+            this.dialogVisible = false;
+            res.data.list.forEach((value, index, array) => {
+              this.tableData.push({ phone_number: value });
+            });
           } else {
-            this.tableData = [];
+            this.dialog_text =
+              params.dialog_query_failed_text_require_reason + res.message;
+            this.button_text = params.dialog_button_failed;
           }
         },
         fail: (res) => {
-          this.dialogVisible = false;
+          this.dialog_text = params.dialog_query_failed_text;
           this.tableData = [];
         },
       });
+    },
+    radioChangeEvent({ row }) {
+      this.selectRow = row;
+      console.log("单选事件");
+    },
+    clearRadioRowEvent() {
+      this.selectRow = null;
+      this.$refs.xTable1.clearRadioRow();
     },
   },
 };
--- src/golbal.js
+++ src/golbal.js
@@ -3,9 +3,21 @@ const api_send_sms = API_Host + "/SMS/send_sms";
 const api_get_token = API_Host + "/SMS/get_token";
 const api_query_phone_number = API_Host + "/business/query_phone_number";
 
+const dialog_loading_text = "正在查询...";
+const dialog_query_failed_text_require_reason = "访问失败,失败原因:";
+const dialog_query_failed_text = "网络访问失败，请稍后重试！";
+
+const dialog_button_loading = "强制停止";
+const dialog_button_failed = "关闭";
+
 export default {
   API_Host,
   api_send_sms,
   api_get_token,
   api_query_phone_number,
+  dialog_loading_text,
+  dialog_query_failed_text_require_reason,
+  dialog_query_failed_text,
+  dialog_button_loading,
+  dialog_button_failed,
 };
